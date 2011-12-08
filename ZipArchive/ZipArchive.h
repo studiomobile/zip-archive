dec4a72d9161fc629a26d20d34f9d42458ed8777
Sergey Martynov <martynovs@gmail.com>
2011-12-08 19:38:01 +0300

    Initial Commit

---
+++ ZipArchive/ZipArchive.h
@@ -0,0 +1,13 @@
+//
+//  ZipArchive.h
+//  ZipArchive
+//
+//  Created by Sergey Martynov on 08.12.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZipArchive : NSObject
+
+@end
---
+++ ZipArchive/ZipArchive.m
@@ -0,0 +1,23 @@
+//
+//  ZipArchive.m
+//  ZipArchive
+//
+//  Created by Sergey Martynov on 08.12.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ZipArchive.h"
+
+@implementation ZipArchive
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
